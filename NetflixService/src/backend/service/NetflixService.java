962d5b6c4f5957ce4dbcf26cef2a7f27bddae8c3
SophoclesB <sophoclesb@yahoo.com>
2025-04-06 00:38:40 +0300

    Finally finished

--- NetflixService/src/backend/service/NetflixService.java
+++ NetflixService/src/backend/service/NetflixService.java
@@ -21,31 +21,75 @@ public class NetflixService {
 		if (subscriber.getFavoriteCount() == 0)
 			return null;
 		int movieCount = 0;
-		for (int i = 0; i < contentRepo.getAllContent().length; i++) {
-			if (contentRepo.getAllContent()[i] instanceof Movie)
+
+		Content[] allContent = contentRepo.getAllContent();
+
+		for (int i = 0; i < allContent.length; i++) {
+			if (allContent[i] instanceof Movie)
 				movieCount++;
+			else
+				allContent[i] = null;
 		}
+
 		Movie[] allMovies = new Movie[movieCount];
 		for (int i = 0; i < movieCount; i++) {
-			if (contentRepo.getAllContent()[i] instanceof Movie)
-				allMovies[i] = (Movie) contentRepo.getAllContent()[i];
+			for (int j = 0; j < allContent.length; j++) {
+				if (allContent[j] != null) {
+					allMovies[i] = (Movie) allContent[j];
+					allContent[j] = null;
+					break;
+				}
+			}
 		}
 
-		Algos.quickSort(allMovies, 0, allMovies.length - 1);
+		Algos.quickSort(allMovies, 0, allMovies.length - 1); // Sort by ID (Recency)
+
+		int genreCount = 0;
+		outer: for (int i = allMovies.length - 1; i >= 0; i--) {
+			for (int j = i + 1; j < allMovies.length; j++) {
+				if (allMovies[j] != null && allMovies[i].getPrimaryGenre().equals(allMovies[j].getPrimaryGenre())) {
+					allMovies[i] = null;
+					continue outer;
+				}
+			}
+			genreCount++;
+		}
+
+		Movie[] uniqueGenreMovies = new Movie[genreCount];
+		genreCount = 0;
+		for (int i = allMovies.length - 1; i >= 0; i--) {
+			if (allMovies[i] != null)
+				uniqueGenreMovies[genreCount++] = allMovies[i];
+		}
 
 		int recCount = 0;
-		Movie[] recommendedMovies = new Movie[maxResults];
-		for (int i = allMovies.length - 1; i >= 0; i--) {
-			if (recCount <= maxResults) {
-				for (int j = 1; j < allMovies.length; j++) {
-				}
-			} else
+		Movie[] recommendedMovies = new Movie[(genreCount <= maxResults) ? genreCount : maxResults];
+		for (int i = 0; i < maxResults; i++) {
+			recommendedMovies[i] = uniqueGenreMovies[i];
+			if (i >= uniqueGenreMovies.length - 1)
 				break;
 		}
+
+		for (int i = 0; i < uniqueGenreMovies.length; i++) {
+			for (int j = 0; j < subscriber.getFavorites().length; j++) {
+				if (uniqueGenreMovies[i].getPrimaryGenre().equals(subscriber.getFavorites()[j])) {
+					for (int k = recommendedMovies.length - 1; k > recCount; k--) {
+						recommendedMovies[k] = recommendedMovies[k - 1];
+					}
+					recommendedMovies[recCount] = uniqueGenreMovies[i];
+					recCount++;
+				}
+			}
+		}
+
 		return recommendedMovies;
 	}
 
 	public void printRecommendations(Content[] recommendedMovies, Subscriber requestsRecs) {
-
+		System.out.println("Movies recommended for Subscriber " + requestsRecs.getSubscriberEmail());
+		for (int i = 0; i < recommendedMovies.length; i++) {
+			System.out.println(
+					"- " + recommendedMovies[i].getTitle() + "(? " + recommendedMovies[i].getAverageRating() + ")");
+		}
 	}
 }
